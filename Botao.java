b42ecb36ccef4873816cb774b212eceb5bb3b107
Marcos Vinicius Sombra <markos.vs@live.com>
2017-03-31 16:22:10 -0300

    Add files via upload

---
+++ Botao.java
@@ -0,0 +1,35 @@
+package parte2joy;
+
+import java.util.ArrayList;
+
+public class Botao {
+    //atributos
+    private ArrayList<String> acoes;
+    private String id;
+    
+    //construtor
+    public Botao(String id) {
+        this.id = id;
+        acoes = new ArrayList<>();
+    }
+        
+    //getters
+    public ArrayList<String> getAcoes() {
+        return acoes;
+    }
+
+    public String getId() {
+        return id;
+    }
+    
+    //setters
+    public void setAcao(ArrayList<String> acoes) {
+        this.acoes = acoes;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    
+}
